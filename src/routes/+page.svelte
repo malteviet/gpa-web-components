655747d9504097376f2a79804abe5357b923243a
Nico Beierle <nico.beierle@gmail.com>
2024-09-05 14:03:28 +0200

    feat: export setEntries func to window & add component route

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,5 @@
-<script lang="ts">
-	import Component from '../lib/Component.svelte';
-</script>
-
-<h1>gpa-web-components</h1>
-<p>
-	Welcome to <a href="https://github.com/beierle-krey/gpa-web-components">Drag & Drop</a> from html to
-	C#.
-</p>
-<Component />
+<ul>
+	<li>
+		<a href="./device-lib">Device Lib</a>
+	</li>
+</ul>
---
+++ src/routes/device-lib/+page.svelte
@@ -0,0 +1,21 @@
+<script lang="ts" module>
+	/*
+	declare global {
+		interface Window {
+			component?: Component;
+		}
+	}
+    */
+</script>
+
+<script lang="ts">
+	import Component from '$lib/Component.svelte';
+
+	let component = $state<Component>();
+
+	$effect(() => {
+		Object.assign(window, { component });
+	});
+</script>
+
+<Component bind:this={component} />
